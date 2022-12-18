144e1fb10688fe6fd8552a0519182031dd820bb6
rishuriya <rdrishav2002@yahoo.com>
2022-12-19 02:10:47 +0530

    template:added form to fill data

--- aashram_sewa/user/admin.py
+++ aashram_sewa/user/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
 from user.models import user_form
-
-admin.site.register(user_form)
+class UserFormAdmin(admin.ModelAdmin):
+    list_display=('email', 'phone','date_of_arrival','time_of_help','duration_of_help','age','sleep_dormitory')
+    list_filter = ['date_of_arrival','time_of_help','duration_of_help','age','sleep_dormitory']
+admin.site.register(user_form, UserFormAdmin)
 
 # Register your models here.
--- aashram_sewa/user/views.py
+++ aashram_sewa/user/views.py
@@ -4,4 +4,8 @@ from .forms import UserForm
 def seva_form(request):
     context ={}
     context['form']= UserForm()
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
     return render(request, 'user/form.html', context)
